9c6e4f8f50534a85e86f0c285e61e11dc2d7a1cd
vanaklay <ven2kaze@gmail.com>
2022-05-05 10:21:43 +0200

    update func to pure func

--- 02-water/exo_12.js
+++ 02-water/exo_12.js
@@ -1,15 +1,16 @@
 import { getValidList } from '../utils/index.js';
 
 const getMinimumDistance = (numbers) => {
-  const sortedNumbers = numbers.sort((a, b) => a - b);
-  let difference = Number.MAX_SAFE_INTEGER;
-  for (let i = 0; i < sortedNumbers.length; i++) {
-    if (i === sortedNumbers.length - 1) break;
-    if (sortedNumbers[i + 1] - sortedNumbers[i] < difference) {
-      difference = sortedNumbers[i + 1] - sortedNumbers[i];
+  // Avoid mutated
+  const sortedNumbers = [...numbers].sort((a, b) => a - b);
+
+  return sortedNumbers.reduce((difference, num, i, array) => {
+    if (i === array.length) return;
+    if (array[i + 1] - num < difference) {
+      difference = array[i + 1] - num;
     }
-  }
-  return difference;
+    return difference;
+  }, Number.MAX_SAFE_INTEGER);
 };
 
 const main = () => {
